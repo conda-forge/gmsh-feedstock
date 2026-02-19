075d672466cfd15e51ff70d635dd4577e81896b9
Claudius Haag <claudius.haag@web.de>
2026-02-19 21:55:40 +0100

    Copy METADATA file

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -1,2 +1,3 @@
 mkdir %SP_DIR%
 copy api\gmsh.py %SP_DIR%\gmsh.py
+copy  build\METADATA %SP_DIR%\gmsh-%PKG_VERSION%.dist-info
--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -4,3 +4,4 @@ set -ex
 
 mkdir -p ${SP_DIR}
 cp api/gmsh.py ${SP_DIR}/gmsh.py
+cp  build/METADATA ${SP_DIR}/gmsh-${PKG_VERSION}.dist-info
