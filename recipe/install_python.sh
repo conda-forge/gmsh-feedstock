5c4e9fc326699b022acb4c0232f3b364cb60c890
Claudius Haag <claudius.haag@mib.uni-stuttgart.de>
2026-02-24 13:17:19 +0100

    Clean up script

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -4,5 +4,5 @@ set -ex
 
 mkdir -p ${SP_DIR}
 cp api/gmsh.py ${SP_DIR}/gmsh.py
-mkdir -p "${SP_DIR}/gmsh-${PKG_VERSION}.dist-info" && \
+mkdir -p "${SP_DIR}/gmsh-${PKG_VERSION}.dist-info"
 cp build/METADATA "${SP_DIR}/gmsh-${PKG_VERSION}.dist-info/METADATA"
