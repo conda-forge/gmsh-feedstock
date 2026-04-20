c62fa991cc6aae4034a1cd0019eda91385fe24a7
Olivier Tremblay-Noël <onoel@nanoacademic.com>
2026-04-20 12:59:28 -0400

    adding flags

--- recipe/bld_gmsh.bat
+++ recipe/bld_gmsh.bat
@@ -6,10 +6,21 @@ set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 mkdir build
 cd build
 
+if "%openmp_impl%" == "intel" (
+    set "OPENMP_ARGS=-DOpenMP_CXX_LIB_NAMES=libiomp5md"
+    set "OPENMP_ARGS=%OPENMP_ARGS% -DOpenMP_CXX_FLAGS=/clang:-fopenmp=libiomp5"
+    set "OPENMP_ARGS=%OPENMP_ARGS% -DOpenMP_libiomp5md_LIBRARY=%LIBRARY_LIB%\libiomp5md.lib"
+    set "CONFIG_ARGS=-D "CMAKE_C_FLAGS=/O2 /arch:AVX2 -DWIN32" -D "CMAKE_CXX_FLAGS=/O2 /arch:AVX2 -DWIN32""
+) else (
+    set "OPENMP_ARGS="
+    set "CONFIG_ARGS="
+)
+
 :: Configure.
 cmake -G "Visual Studio 17 2022" -A x64 -T ClangCL ^
       -D CMAKE_POLICY_VERSION_MINIMUM=3.5 ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      %CONFIG_ARGS% ^
       -D ENABLE_OS_SPECIFIC_INSTALL=OFF ^
       -D ENABLE_BUILD_DYNAMIC=ON ^
       -D ENABLE_BUILD_SHARED=ON ^
@@ -18,21 +29,14 @@ cmake -G "Visual Studio 17 2022" -A x64 -T ClangCL ^
       -D BLAS_LAPACK_LIBRARIES=%LIBRARY_PREFIX%\lib\lapack.lib;%LIBRARY_PREFIX%\lib\blas.lib ^
       -D GMSH_RELEASE=1 ^
       -D ENABLE_OPENMP=ON ^
+      %OPENMP_ARGS% ^
       -D ENABLE_CAIRO=1 ^
       -D ENABLE_MED=1 ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
-:: Build.
-cmake --build . --target package
-if errorlevel 1 exit 1
-
-:: Test.
-:: ctest
-:: if errorlevel 1 exit 1
-
-:: Install.
-cmake --build . --target install
+:: Build and Install.
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 del %LIBRARY_PREFIX%\lib\gmsh.py
